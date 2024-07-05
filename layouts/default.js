91222cbe6afdb8650023e43ca2c71e9637990e77
KostuX <77516813+KostuX@users.noreply.github.com>
2024-07-05 14:03:26 +0100

    Update default.js

--- layouts/default.js
+++ layouts/default.js
@@ -9,18 +9,14 @@ export default function DefaultLayout({ children }) {
     {
       const lenis = new Lenis();
 
-      lenis.on("scroll", (e) => {
-        console.log(e);
-      });
-
       function raf(time) {
         lenis.raf(time);
         requestAnimationFrame(raf);
       }
-
       requestAnimationFrame(raf);
     }
   }, []);
+
   return (
     <div>
       <Head />
