2d2bb4e8fcbd72d596d704e4c1542c5abbd64012
KostuX <77516813+KostuX@users.noreply.github.com>
2024-07-11 10:37:44 +0100

    log fix

--- pages/api/LoggerAPI.js
+++ pages/api/LoggerAPI.js
@@ -8,13 +8,11 @@ import log_write from "../../lib/logs/LOG_write";
 export default withIronSessionApiRoute(LoggerAPI, ironOptions);
 
 async function LoggerAPI(req, res) {
-
-  res.status(200).send();
- 
+  res.status(200).send(); 
   let data = req.body;
 
-  if (data?.type === "add") {
-    log_write("INFO", `[*] ${data.data.type} - ${data.data.data}`);
+  if (data?.type === "add") { 
+    log_write("INFO", `[*] ${data.data.type} - ${data.data.data.ip}`);
     if (data.data.type === "connected") {
       userConnected(data.data);
     } 
