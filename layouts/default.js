c3a8df4129c40e7e421197c11c3ad40e389c3ccc
KostuX <77516813+KostuX@users.noreply.github.com>
2024-12-03 00:41:09 +0000

    vercel analytics

--- layouts/default.js
+++ layouts/default.js
@@ -2,9 +2,16 @@ import { Navbar } from "../components/navbar";
 import { Foot } from "../components/footer";
 import { Head } from "../components/head";
 import ScrollContext from "./ScrollContext";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import React, { useState, useEffect } from "react";
 
 export default function DefaultLayout({ children }) {
+  const [isHydrated, setIsHidrated] = useState(false);
+
+  useEffect(() => {
+    setIsHidrated(true);
+  }, []);
   return (
     <div>
       <Head />
@@ -12,6 +19,8 @@ export default function DefaultLayout({ children }) {
       <ScrollContext>
         <main className=""> {children}</main>
       </ScrollContext>
+      {isHydrated && <SpeedInsights />}
+      <Analytics />
     </div>
   );
 }
