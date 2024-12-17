115c8ac5740f2548162fb63633a03d5d71cb641b
KostuX <77516813+KostuX@users.noreply.github.com>
2024-12-17 00:26:56 +0000

    project template

--- layouts/projects_layout.js
+++ layouts/projects_layout.js
@@ -11,14 +11,12 @@ export const Projects_layout = (prop) => {
     const project = prop.project
 
     return (
-        <div className="m-12">
-            <div className={`text-4xl font-bold text-center ${neue.className}`}>{project.title}</div>
-            <div className="text-center my-10">
-                {" "}
+        <div className="mt-12 mb-12 text-center">
+            <div className={`text-4xl font-bold ${neue.className}`}>{project.title}</div>
+            <div className="mx-4">                
                 <div>
-                    <b>Project:</b>{" "}
-                    <a className="underline" href={project.link} target="_blank">
-                        {" "}
+                    <b>Project: </b>
+                    <a className="underline" href={project.link} target="_blank">                        
                         {project.link}
                     </a>
                 </div>
@@ -26,11 +24,11 @@ export const Projects_layout = (prop) => {
                     <b>Language:</b> {project.language}
                 </div>
                 <div>
-                    <b>Purpose:</b> {project.description_short}
+                    <b>Purpose: </b> {project.description_short}
                 </div>
             </div>
-            <div className="  mx-5 flex justify-center ">
-                <div className="max-w-4xl w-5/6   ">
+            <div className="mt-12 flex justify-center">
+                <div className="max-w-4xl w-5/6">
                     <EmblaCarousel
                         slides={project.images}
                         options={OPTIONS}
@@ -38,8 +36,8 @@ export const Projects_layout = (prop) => {
                     />
                 </div>
             </div>
-            <div className="w-screen  flex justify-center ">
-                <div className="mt-6 mx-10 text-lg max-w-4xl  text-center">
+            <div className="flex justify-center">
+                <div className="mx-4 mt-6 text-lg max-w-4xl text-center">
                     {project.description}
                 </div>
             </div>
